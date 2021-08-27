5a24046f945f32c1fb2a0a4300a67cdaccb4c21d
khloe-r <khloe.ramdhan@outlook.com>
2021-08-26 22:59:10 -0400

    added search bar + fixed graph spacing

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from datetime import datetime
 import plotly
 import plotly.graph_objs as go
@@ -54,63 +54,68 @@ def create_plot(stars):
 def index():
     return render_template('index.html')
 
-@app.route('/results')
+@app.route('/results', methods = ['POST', 'GET'])
 def results():
-    UserURL = requests.get("https://api.github.com/users/khloe-r")
-    user_result = UserURL.json()
-    languages = {}
-    stars = {}
-    repo_dates = []
-    repos = []
-    date = datetime.strptime(user_result['created_at'][0:10], '%Y-%m-%d')
-    formatted_date = date.strftime("%B %d, %Y")
-    ReposURL = requests.get(user_result['repos_url'])
-    repo_result = ReposURL.json()
-    for repo in repo_result:
-        repo_dates.append(repo['updated_at'][0:10])
-        LanguageURL = requests.get(repo['languages_url'])
-        language_result = LanguageURL.json()
-        for lang in language_result:
-            if lang in languages:
-                languages[lang] += 1 #language_result[lang]
-                stars[lang] += int(repo['stargazers_count'])
-            else:
-                languages[lang] = 1 #language_result[lang]
-                stars[lang] = int(repo['stargazers_count'])
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form.to_dict()
+        username = form_data['username_value']
+        UserURL = requests.get("https://api.github.com/users/%s" % username) 
+        user_result = UserURL.json()
+        languages = {}
+        stars = {}
+        repo_dates = []
+        repos = []
+        date = datetime.strptime(user_result['created_at'][0:10], '%Y-%m-%d')
+        formatted_date = date.strftime("%B %d, %Y")
+        ReposURL = requests.get(user_result['repos_url'])
+        repo_result = ReposURL.json()
+        for repo in repo_result:
+            repo_dates.append(repo['updated_at'][0:10] + " " + repo['updated_at'][11:19])
+            LanguageURL = requests.get(repo['languages_url'])
+            language_result = LanguageURL.json()
+            for lang in language_result:
+                if lang in languages:
+                    languages[lang] += 1 #language_result[lang]
+                    stars[lang] += int(repo['stargazers_count'])
+                else:
+                    languages[lang] = 1 #language_result[lang]
+                    stars[lang] = int(repo['stargazers_count'])
 
-    sorted = list(repo_dates)
-    sorted.sort(key = lambda date: datetime.strptime(date, '%Y-%m-%d'), reverse=True)
-    for i in range(min(len(sorted), 4)):
-        repo_data = repo_result[repo_dates.index(sorted[i])]
-        repos.append({
-            'name': repo_data['name'],
-            'lang': repo_data['language'],
-            'descript': repo_data['description'],
-            'forks': repo_data['forks_count'],
-            'stars': repo_data['stargazers_count'],
-            'watchers': repo_data['watchers_count'],
-            'view': repo_data['html_url'],
-            'clone': repo_data['clone_url'],
-            'updated': repo_data['updated_at'][0:10]
-            })
+        sorted = list(repo_dates)
+        sorted.sort(key = lambda date: datetime.strptime(date, '%Y-%m-%d %H:%M:%S'), reverse=True)
+        for i in range(min(len(sorted), 4)):
+            repo_data = repo_result[repo_dates.index(sorted[i])]
+            repos.append({
+                'name': repo_data['name'],
+                'lang': repo_data['language'],
+                'descript': repo_data['description'],
+                'forks': repo_data['forks_count'],
+                'stars': repo_data['stargazers_count'],
+                'watchers': repo_data['watchers_count'],
+                'view': repo_data['html_url'],
+                'clone': repo_data['clone_url'],
+                'updated': repo_data['updated_at'][0:10]
+                })
 
-    pie = create_pie(languages)
-    bar = create_plot(stars)
-    return render_template(
-        'results.html',
-        username=user_result['login'],
-        img=user_result['avatar_url'],
-        name=user_result['name'],
-        bio=user_result['bio'],
-        web=user_result['blog'],
-        followers=user_result['followers'],
-        following=user_result['following'],
-        repos=user_result['public_repos'],
-        date=formatted_date,
-        plot=pie,
-        bar=bar,
-        dates=repos,
-        sorted=sorted
-        )
+        pie = create_pie(languages)
+        bar = create_plot(stars)
+        return render_template(
+            'results.html',
+            username=user_result['login'],
+            img=user_result['avatar_url'],
+            name=user_result['name'],
+            bio=user_result['bio'],
+            web=user_result['blog'],
+            followers=user_result['followers'],
+            following=user_result['following'],
+            repos=user_result['public_repos'],
+            date=formatted_date,
+            plot=pie,
+            bar=bar,
+            dates=repos,
+            sorted=sorted
+            )
 
 
