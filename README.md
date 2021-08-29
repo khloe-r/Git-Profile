907b6909bcc118ff86793ac2d0635558437e7cf8
khloe-r <khloe.ramdhan@outlook.com>
2021-08-29 19:50:49 -0400

    error page added

---
+++ README.md
@@ -0,0 +1,14 @@
+# GitProfile
+
+Github Profile viewer built using `Flask` for the backend and `HTML`/`CSS` templates for the frontend.
+
+## Features
+
+- Searches `Github API` for user profile and returns data in an easy-to-view format
+- Displays most recent repositories and other profile information using `Plotly` graphs
+- Includes error page when users are not found
+
+## Preview
+
+<img width="1367" alt="Screen Shot 2021-08-29 at 5 36 31 PM" src="https://user-images.githubusercontent.com/75541476/131266146-82a99f71-6c44-4503-ac24-360e8436a90e.png">
+<img width="1421" alt="Screen Shot 2021-08-29 at 7 49 09 PM" src="https://user-images.githubusercontent.com/75541476/131269373-75dbdb58-a867-455e-bf00-a2a2985b1dee.png">
--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import json
 
 app = Flask(__name__)
 
+# Languages Used Graph
 def create_pie(langs):
 
     N = 5
@@ -32,6 +33,7 @@ def create_pie(langs):
     graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
 
+# Stars per Language Graph
 def create_plot(stars):
 
     N = 5
@@ -50,73 +52,80 @@ def create_plot(stars):
     graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
 
-@app.route('/')
+@app.route('/form')
 def index():
     return render_template('index.html')
 
 @app.route('/results', methods = ['POST', 'GET'])
 def results():
     if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+        return render_template('error.html', error="The URL /results cannot be accessed directly. Try going to '/form' to submit form first!")
     if request.method == 'POST':
         form_data = request.form.to_dict()
         username = form_data['username_value']
         UserURL = requests.get("https://api.github.com/users/%s" % username) 
         user_result = UserURL.json()
-        languages = {}
-        stars = {}
-        repo_dates = []
-        repos = []
-        date = datetime.strptime(user_result['created_at'][0:10], '%Y-%m-%d')
-        formatted_date = date.strftime("%B %d, %Y")
-        ReposURL = requests.get(user_result['repos_url'])
-        repo_result = ReposURL.json()
-        for repo in repo_result:
-            repo_dates.append(repo['updated_at'][0:10] + " " + repo['updated_at'][11:19])
-            LanguageURL = requests.get(repo['languages_url'])
-            language_result = LanguageURL.json()
-            for lang in language_result:
-                if lang != "message" and lang != "documentation_url":
-                    if lang in languages:
-                        languages[lang] += 1 #language_result[lang]
-                        stars[lang] += int(repo['stargazers_count'])
-                    else:
-                        languages[lang] = 1 #language_result[lang]
-                        stars[lang] = int(repo['stargazers_count'])
+        if 'message' not in user_result:
+            languages = {}
+            stars = {}
+            repo_dates = []
+            repos = []
+            date = datetime.strptime(user_result['created_at'][0:10], '%Y-%m-%d')
+            formatted_date = date.strftime("%B %d, %Y")
+            ReposURL = requests.get(user_result['repos_url'])
+            repo_result = ReposURL.json()
+            for repo in repo_result:
+                repo_dates.append(repo['updated_at'][0:10] + " " + repo['updated_at'][11:19])
+                LanguageURL = requests.get(repo['languages_url'])
+                language_result = LanguageURL.json()
+                for lang in language_result:
+                    if lang != "message" and lang != "documentation_url":
+                        if lang in languages:
+                            languages[lang] += 1 #language_result[lang]
+                            stars[lang] += int(repo['stargazers_count'])
+                        else:
+                            languages[lang] = 1 #language_result[lang]
+                            stars[lang] = int(repo['stargazers_count'])
 
-        sorted = list(repo_dates)
-        sorted.sort(key = lambda date: datetime.strptime(date, '%Y-%m-%d %H:%M:%S'), reverse=True)
-        for i in range(min(len(sorted), 4)):
-            repo_data = repo_result[repo_dates.index(sorted[i])]
-            repos.append({
-                'name': repo_data['name'],
-                'lang': repo_data['language'],
-                'descript': (repo_data['description'] if repo_data['description'] != "None" else ""),
-                'forks': repo_data['forks_count'],
-                'stars': repo_data['stargazers_count'],
-                'watchers': repo_data['watchers_count'],
-                'view': repo_data['html_url'],
-                'clone': repo_data['clone_url'],
-                'updated': datetime.strptime(repo_data['updated_at'][0:10], '%Y-%m-%d').strftime("%B %d, %Y")
-                })
+            sorted = list(repo_dates)
+            sorted.sort(key = lambda date: datetime.strptime(date, '%Y-%m-%d %H:%M:%S'), reverse=True)
+            for i in range(min(len(sorted), 4)):
+                repo_data = repo_result[repo_dates.index(sorted[i])]
+                repos.append({
+                    'name': repo_data['name'],
+                    'lang': repo_data['language'],
+                    'descript': (repo_data['description'] if repo_data['description'] != "None" else ""),
+                    'forks': repo_data['forks_count'],
+                    'stars': repo_data['stargazers_count'],
+                    'watchers': repo_data['watchers_count'],
+                    'view': repo_data['html_url'],
+                    'clone': repo_data['clone_url'],
+                    'updated': datetime.strptime(repo_data['updated_at'][0:10], '%Y-%m-%d').strftime("%B %d, %Y")
+                    })
 
-        pie = create_pie(languages)
-        bar = create_plot(stars)
-        return render_template(
-            'results.html',
-            username=user_result['login'],
-            img=user_result['avatar_url'],
-            name=user_result['name'],
-            bio=user_result['bio'],
-            web=user_result['blog'],
-            followers=user_result['followers'],
-            following=user_result['following'],
-            repos=user_result['public_repos'],
-            date=formatted_date,
-            plot=pie,
-            bar=bar,
-            dates=repos,
-            sorted=sorted
-            )
+            pie = create_pie(languages)
+            bar = create_plot(stars)
+            return render_template(
+                'results.html',
+                username=user_result['login'],
+                img=user_result['avatar_url'],
+                name=user_result['name'],
+                bio=user_result['bio'],
+                web=user_result['blog'],
+                followers=user_result['followers'],
+                following=user_result['following'],
+                repos=user_result['public_repos'],
+                date=formatted_date,
+                plot=pie,
+                bar=bar,
+                dates=repos,
+                sorted=sorted
+                )
+        else:
+            if (user_result['message'][0:14] == 'API rate limit'):
+                error = "API limit exceeded!"
+            else:
+                error = "User not found!"
+            return render_template('error.html', error=error)
 
 
